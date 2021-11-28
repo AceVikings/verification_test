62f9f9fc505df93c3f4c38960e72af91401fd55b
akashjha011 <51465755+akashjha011@users.noreply.github.com>
2021-11-28 11:11:23 +0530

    Create 1_Storage.sol

---
+++ 1_Storage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+
+    event numberStored(uint256 indexed number);
+    uint256 number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+        emit numberStored(num);
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
